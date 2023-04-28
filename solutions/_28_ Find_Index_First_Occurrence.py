918df2a5248b42d702855829962634e937536569
andrew_cherry <chernyaevav@gmail.com>
2023-04-28 21:28:40 +0300

    28. Find index first Substring (встроенный find)

--- solutions/_28_ Find_Index_First_Occurrence.py
+++ solutions/_28_ Find_Index_First_Occurrence.py
@@ -1,52 +1,55 @@
 # def strStr(haystack: str, needle: str) -> int:
+#     n = len(needle)
 #     for idx, char in enumerate(haystack):
-#         if char == needle[0] and haystack[idx:idx+len(needle)] == needle:
+#         if char == needle[0] and haystack[idx:idx + n] == needle:
 #             return idx
 #     return -1
 
-
 def strStr(haystack: str, needle: str) -> int:
-    m = len(needle)
-    n = len(haystack)
-    RADIX = 26
-    MOD = 1_000_000_033
-    MAX_WEIGHT = 1
+    return haystack.find(needle)
 
-    if n < m:
-      return -1
-
-    for _ in range(m):
-        MAX_WEIGHT = (MAX_WEIGHT * RADIX) % MOD
-
-    def hash_value(string):
-        ans = 0
-        factor = 1
-
-        for i in range(m - 1, -1, -1):
-          ans += ((ord(string[i]) - 97) * (factor)) % MOD
-          factor = (factor * RADIX) % MOD
-
-        return ans % MOD
-
-    hash_needle = hash_value(needle)
-
-    for window_start in range(n - m + 1):
-        if window_start == 0:
-          hash_hay = hash_value(haystack)
-        else:
-            hash_hay = ((hash_hay * RADIX) % MOD
-                      - ((ord(haystack[window_start - 1]) - 97)
-                         * MAX_WEIGHT) % MOD
-                      + (ord(haystack[window_start + m - 1]) - 97)
-                      + MOD) % MOD
-
-        if hash_needle == hash_hay:
-            for i in range(m):
-                if needle[i] != haystack[i + window_start]:
-                  break
-            if i == m - 1:
-              return window_start
-    return -1
+# def strStr(haystack: str, needle: str) -> int:
+#     m = len(needle)
+#     n = len(haystack)
+#     RADIX = 26
+#     MOD = 1_000_000_033
+#     MAX_WEIGHT = 1
+#
+#     if n < m:
+#       return -1
+#
+#     for _ in range(m):
+#         MAX_WEIGHT = (MAX_WEIGHT * RADIX) % MOD
+#
+#     def hash_value(string):
+#         ans = 0
+#         factor = 1
+#
+#         for i in range(m - 1, -1, -1):
+#           ans += ((ord(string[i]) - 97) * (factor)) % MOD
+#           factor = (factor * RADIX) % MOD
+#
+#         return ans % MOD
+#
+#     hash_needle = hash_value(needle)
+#
+#     for window_start in range(n - m + 1):
+#         if window_start == 0:
+#           hash_hay = hash_value(haystack)
+#         else:
+#             hash_hay = ((hash_hay * RADIX) % MOD
+#                       - ((ord(haystack[window_start - 1]) - 97)
+#                          * MAX_WEIGHT) % MOD
+#                       + (ord(haystack[window_start + m - 1]) - 97)
+#                       + MOD) % MOD
+#
+#         if hash_needle == hash_hay:
+#             for i in range(m):
+#                 if needle[i] != haystack[i + window_start]:
+#                   break
+#             if i == m - 1:
+#               return window_start
+#     return -1
 
 if __name__ == '__main__':
     haystack = "sadbutsad"
@@ -68,3 +71,7 @@ if __name__ == '__main__':
     haystack = "abc"
     needle = "c"
     assert strStr(haystack, needle) == 2
+
+    haystack = "mississipi"
+    needle = "issipi"
+    assert strStr(haystack, needle) == 4
