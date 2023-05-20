672bbc81b2d5fc9c2293c5a2ba6b5c9d7c90ce9a
andrew_cherry <chernyaevav@gmail.com>
2023-05-20 19:33:22 +0300

    36. Valid Sudoku (refactoring code)

--- solutions/_36_Valid_sudoku.py
+++ solutions/_36_Valid_sudoku.py
@@ -1,74 +1,59 @@
 from typing import List
 
+import pytest
+
 
 def isValidSudoku(board: List[List[str]]) -> bool:
+    rows = [[] for _ in range(9)]
+    columns = [[] for _ in range(9)]
+    blocks = [[[] for _ in range(3)] for _ in range(3)]
 
-    def is_valid_row(n):
-        for i in range(n):
-            valid_list = []
-            for j in range(n):
-                if board[i][j] == ".":
-                    continue
-                if board[i][j] in valid_list:
-                    return False
-                valid_list.append(board[i][j])
-        return True
-
-    def is_valid_col(n):
-        for i in range(n):
-            valid_list = []
-            for j in range(n):
-                if board[j][i] == ".":
-                    continue
-                if board[j][i] in valid_list:
-                    return False
-                valid_list.append(board[j][i])
-        return True
-
-    def is_valid_board(row: int, col: int):
-        valid_list = []
-        for i in range(row, row + 3):
-            for j in range(col, col + 3):
-                if board[i][j] == ".":
-                    continue
-                if board[i][j] in valid_list:
-                    return False
-                valid_list.append(board[i][j])
-        return True
-
-    for col in [0, 3, 6]:
-        for row in [0, 3, 6]:
-            if not is_valid_board(col, row):
+    for i in range(9):
+        for j in range(9):
+            cell = board[i][j]
+            if cell == ".":
+                continue
+            if (    cell in rows[i] or
+                    cell in columns[j] or
+                    cell in blocks[i // 3][j // 3]
+            ):
                 return False
+            rows[i].append(cell)
+            columns[j].append(cell)
+            blocks[i // 3][j // 3].append(cell)
+    return True
 
-    return is_valid_row(9) and is_valid_col(9)
+class TestCase:
+    def test_true(self):
+        self.board = [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ]
+        self.res = True
+        assert isValidSudoku(self.board) == self.res
+
+    def test_false(self):
+        self.board = [
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ]
+        self.res = False
+        assert isValidSudoku(self.board) == self.res
 
 
 if __name__ == '__main__':
-    board = [
-        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-        [".", "9", "8", ".", ".", ".", ".", "6", "."],
-        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-        [".", "6", ".", ".", ".", ".", "2", "8", "."],
-        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-    ]
-    res = True
-    assert isValidSudoku(board) == res
-
-    board = [
-        ["8", "3", ".", ".", "7", ".", ".", ".", "."],
-        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-        [".", "9", "8", ".", ".", ".", ".", "6", "."],
-        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-        [".", "6", ".", ".", ".", ".", "2", "8", "."],
-        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-    ]
-    res = False
-    assert isValidSudoku(board) == res
+    pytest.main()
