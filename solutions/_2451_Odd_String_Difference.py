9b2caace863db603fdea17f376c129c893a8b62f
andrew_cherry <chernyaevav@gmail.com>
2023-06-21 23:45:22 +0300

    решение готово + тесты

--- solutions/_2451_Odd_String_Difference.py
+++ solutions/_2451_Odd_String_Difference.py
@@ -1,18 +1,35 @@
 from typing import List
 
+import pytest
+
 
 class Solution:
     def oddString(self, words: List[str]) -> str:
         res = {}
         for item in words:
-            diff = []
+            temp_diff = []
             for idx in range(1, len(item)):
-                diff.append(ord(item[idx]) - ord(item[idx-1]))
-            if diff not in res.values():
-                res[diff] = res.get(diff, [])
-        return res
+                temp_diff.append(ord(item[idx]) - ord(item[idx-1]))
+            diff = tuple(temp_diff)
+            res[diff] = res.get(diff, []) + [item]
 
+        for val in res.values():
+            if len(val) == 1:
+                return val[0]
+
+
+class TestCase:
+    def test1(self):
+        words = ["adc", "wzy", "abc"]
+        answer = "abc"
+        assert Solution().oddString(words) == answer
+
+    def test2(self):
+        words = ["aaa", "bob", "ccc", "ddd"]
+        answer = "bob"
+        assert Solution().oddString(words) == answer
 
 if __name__ == '__main__':
-    words = ["adc", "wzy", "abc"]
-    print(Solution().oddString(words))
+    # words = ["adc", "wzy", "abc"]
+    # print(Solution().oddString(words))
+    pytest.main()
