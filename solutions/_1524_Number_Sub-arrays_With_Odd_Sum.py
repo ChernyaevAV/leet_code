be94308ef0871428b7b457421f61e6cb637a2ffd
andrew_cherry <chernyaevav@gmail.com>
2023-06-26 23:00:55 +0300

    задача решена

--- solutions/_1524_Number_Sub-arrays_With_Odd_Sum.py
+++ solutions/_1524_Number_Sub-arrays_With_Odd_Sum.py
@@ -2,14 +2,28 @@ from typing import List
 
 
 class Solution:
+    # def numOfSubarrays(self, arr: List[int]) -> int:
+    #     ans = 0
+    #     for i in range(len(arr)):
+    #         sub_arr_sum = 0
+    #         for j in range(i, len(arr)):
+    #             sub_arr_sum += arr[j]
+    #             if sub_arr_sum % 2 != 0:
+    #                 ans += 1
+    #     return ans
+
     def numOfSubarrays(self, arr: List[int]) -> int:
-        ans = 0
-        for i in range(len(arr)):
-            sub_arr_sum = 0
-            for j in range(i, len(arr)):
-                sub_arr_sum += arr[j]
-                if sub_arr_sum % 2 != 0:
-                    ans += 1
+        mod = 10 ** 9 + 7
+        odd_sum, even_sum, curr_sum, ans = 0, 1, 0, 0
+        for i in arr:
+            curr_sum += i
+            if curr_sum % 2 == 1:
+                odd_sum += 1
+                ans += even_sum % mod
+            else:
+                even_sum += 1
+                ans += odd_sum % mod
+        ans %= mod
         return ans
 
 
